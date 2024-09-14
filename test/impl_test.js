13e07f2c373b5b1a61150098693eae6a066b985b
James Coglan <jcoglan@gmail.com>
2024-09-14 10:56:33 +0100

    Simple doc store that uses one big JSON structure

---
+++ test/impl_test.js
@@ -0,0 +1,58 @@
+'use strict'
+
+const assert = require('assert').strict
+
+const { MemoryAdapter } = require('../lib/storeroom')
+const JsonFileStore = require('../lib/impls/json_file_store')
+
+function testStore (impl) {
+  let store
+
+  beforeEach(() => {
+    store = impl.createStore()
+  })
+
+  it('returns null for a missing doc', async () => {
+    let doc = await store.get('/doc')
+    assert.equal(doc, null)
+  })
+
+  it('stores a doc', async () => {
+    await store.update('/doc', () => ({ a: 1 }))
+
+    let doc = await store.get('/doc')
+    assert.deepEqual(doc, { a: 1 })
+  })
+
+  it('updates the same doc multiple times', async () => {
+    await Promise.all([
+      store.update('/doc', (doc) => ({ ...doc, a: 1 })),
+      store.update('/doc', (doc) => ({ ...doc, b: 2 })),
+      store.update('/doc', (doc) => ({ ...doc, c: 3 }))
+    ])
+
+    let doc = await store.get('/doc')
+    assert.deepEqual(doc, { a: 1, b: 2, c: 3 })
+  })
+
+  it('updates multiple docs', async () => {
+    await Promise.all([
+      store.update('/doc-a', (doc) => ({ ...doc, a: 1 })),
+      store.update('/doc-b', (doc) => ({ ...doc, b: 2 })),
+      store.update('/doc-c', (doc) => ({ ...doc, c: 3 }))
+    ])
+
+    assert.deepEqual(await store.get('/doc-a'), { a: 1 })
+    assert.deepEqual(await store.get('/doc-b'), { b: 2 })
+    assert.deepEqual(await store.get('/doc-c'), { c: 3 })
+  })
+}
+
+describe('JsonFileStore', () => {
+  testStore({
+    createStore () {
+      let adapter = new MemoryAdapter()
+      return new JsonFileStore(adapter)
+    }
+  })
+})
